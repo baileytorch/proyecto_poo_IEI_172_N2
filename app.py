f2b4d16745083277070d2155a2b7502319ec55d7
baileytorch <baileytorch@hotmail.com>
2025-10-10 16:54:36 -0300

    Obtener datos

--- app.py
+++ app.py
@@ -1,5 +1,27 @@
-from iu.menu_principal import menu_principal, seleccionar_opcion
+# from iu.menu_principal import menu_principal, seleccionar_opcion
 
-while True:
-    menu_principal()
-    seleccionar_opcion()
+# while True:
+#     menu_principal()
+#     seleccionar_opcion()
+from modelos.comuna import Comuna
+from modelos.marca import Marca
+from datos.obtener_datos import obtener_lista_objetos
+
+
+def trabajo_comunas():
+    comuna = Comuna
+    listado_comunas = obtener_lista_objetos(comuna)
+    if listado_comunas:
+        for comuna in listado_comunas:
+            print(f'{comuna.id} {comuna.codigo_comuna} {comuna.nombre_comuna}')
+
+
+def trabajo_marcas():
+    marca = Marca
+    listado_marcas = obtener_lista_objetos(marca)
+    if listado_marcas:
+        for marca in listado_marcas:
+            print(f'{marca.id} {marca.nombre_marca} {marca.pais_origen}')
+
+
+trabajo_marcas()
--- datos/obtener_datos.py
+++ datos/obtener_datos.py
@@ -1,12 +1,10 @@
 from datos.conexion import Session
-from modelos.comuna import Comuna
-from modelos.direccion import Direccion
 from sqlalchemy import func
 
 sesion = Session()
 
 
-def obtener_lista_comunas():
-    listado_comunas = sesion.query(Comuna).all()
-    if len(listado_comunas) > 0:
-        return listado_comunas
+def obtener_lista_objetos(objeto):
+    listado_objetos = sesion.query(objeto).all()
+    if len(listado_objetos) > 0:
+        return listado_objetos
