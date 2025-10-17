e92961c84b59104c1f13218eb81849833e8fc69d
baileytorch <baileytorch@hotmail.com>
2025-10-17 18:25:49 -0300

    metodo gruardar marca

--- datos/guardar_datos.py
+++ datos/guardar_datos.py
@@ -2,6 +2,7 @@ from datos.conexion import Session
 from sqlalchemy import func
 from modelos.comuna import Comuna
 from modelos.marca import Marca
+from modelos.modelo import Modelo
 
 sesion = Session()
 
@@ -26,8 +27,9 @@ def guardar_comuna():
 
 def guardar_marca(marca, pais):
     if marca != '':
-        nueva_marca = Marca(nombre_marca=marca,
-                            pais_origen=pais.title())
+        nueva_marca = Marca(
+            nombre_marca=marca,
+            pais_origen=pais.title())
         sesion.add(nueva_marca)
         try:
             sesion.commit()
@@ -42,25 +44,20 @@ def guardar_marca(marca, pais):
         print('Debe ingresar el nombre de la marca.')
 
 
-# def guardar_modelo():
-#     nombre = input('Ingrese nombre marca: ')
-#     marca = obtener_marca_nombre(nombre)
-#     if not marca:
-#         guardar_marca()
-#     modelo = input('Ingrese nombre del modelo: ')
-#     descripcion = input('Ingrese descripción del modelo: ')
-#     tipo_combustible = input('Ingrese tipo de combustible: ')
-#     combustible = obtener_combustible_nombre(tipo_combustible)
-#     nuevo_modelo = Modelo(
-#         nombre_modelo=modelo.title(),
-#         descripcion_modelo=descripcion.title())
-#     sesion.add(nuevo_modelo)
-#     try:
-#         sesion.commit()
-#         print(
-#             f"La marca '{nuevo_modelo.nombre_marca}' se ha guardado correctamente.")
-#     except Exception as e:
-#         sesion.rollback()
-#         print(f"Error al guardar la marca: {e}")
-#     finally:
-#         sesion.close()
+def guardar_modelo(modelo, descripcion, combustible, cant_puertas, marca):
+    nuevo_modelo = Modelo(
+        nombre_modelo=modelo,
+        descripcion_modelo=descripcion,
+        tipo_combustible=combustible,
+        puertas=cant_puertas,
+        id_marca=marca)
+    sesion.add(nuevo_modelo)
+    try:
+        sesion.commit()
+        print(
+            f"La marca '{nuevo_modelo.nombre_marca}' se ha guardado correctamente.")
+    except Exception as e:
+        sesion.rollback()
+        print(f"Error al guardar la marca: {e}")
+    finally:
+        sesion.close()
